466660f82a1b7b6f946a015221c73ff38c69d1a0
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-11-05 08:15:00 +0530

    inclue

---
+++ DecisionMaking/IFELSEDEMO/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFELSEDEMO
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program to print the words when the number is entered
+            //e.g. input =0 output=zero
+ 
+            //Writing twith consecutive if statements
+
+            int number = 0;
+
+            //if (number == 0)
+            //{
+            //    Console.WriteLine("Zero");
+            //}
+            //if (number == 1)
+            //{
+            //    Console.WriteLine("One");
+            //}
+            //if (number == 2)
+            //{
+            //    Console.WriteLine("Two");
+            //}
+            //if (number == 3)
+            //{
+            //    Console.WriteLine("Three");
+            //}
+
+            // next solution to above problem where all the ifs are executed
+            // is If else
+            //if(number==0)
+            //{
+            //    Console.WriteLine("Zero");
+            //}
+            //else
+            //{
+            //    if(number==1)
+            //    {
+            //        Console.WriteLine("one");
+            //    }
+            //    else
+            //    {
+            //        if(number==2)
+            //        {
+            //            Console.WriteLine("Two");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Three");
+            //        }
+            //    }
+            //}
+
+            // Solution to above complicated if else (nested) is if else if
+
+            if(number==0)
+            {
+                Console.WriteLine("Zero");
+            }
+            else if (number == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (number==2)
+            {
+                Console.WriteLine("Two");
+            }
+            else
+            {
+                Console.WriteLine("Three");
+            }
+
+        }
+    }
+}
