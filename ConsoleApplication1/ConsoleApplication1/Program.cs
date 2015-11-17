9bdac81b035ae3411a68c944d1bc5892198ae2af
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-11-17 08:28:47 +0530

    Console Applications

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValid;
+            //while (isValid)
+            do
+            {
+                Console.WriteLine("enter numbers 0 to 9");
+                String val = Console.ReadLine();
+                int number = int.Parse(val);
+                switch (number)
+                {
+                    case 0:
+                        Console.WriteLine("Zero");
+                        break;
+                    case 1:
+                        Console.WriteLine("one");
+                        break;
+                    default:
+                        Console.WriteLine("other");
+                        break;
+                }
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("given number is even");
+                }
+                else
+                {
+                    Console.WriteLine("given number is odd");
+                }
+                isValid = false;
+                Console.WriteLine("do you want to continue ?press Y to continue any key to exit");
+                String input = Console.ReadLine();
+                //if (input!="y")
+                //{
+                //    break;  
+                //}
+                isValid = (input == "Y" || input == "y");
+            }while(isValid);
+        }
+    }
+}
+
---
+++ ConsoleApplication1/PrimeNumberDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter any number");
+            //string input = Console.ReadLine();
+            DateTime dt = DateTime.Now;
+            int number=2;
+            do
+            {
+                // = int.Parse(input);
+
+                int index = 2;
+                bool isPrime = true;
+                while (index < number/2)
+                {
+                    if (number % index == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                    index++;
+                }
+
+                if (isPrime)
+                {
+                   // Console.WriteLine("Prime Number");
+                    Console.WriteLine(number);
+                }
+                else
+                {
+                   // Console.WriteLine("Not Prime NUmber");
+                }
+                number++;
+            } while (number<100000);
+            
+            Console.WriteLine(dt.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+                TimeSpan tp = DateTime.Now - dt;
+            Console.WriteLine(tp.Seconds+"-"+tp.Milliseconds);
+        }
+    }
+}
