42a70f087363933cc6b68edd9c6c232bb294515a
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-11-24 08:05:29 +0530

    Functions Demo

---
+++ FunctionsDemo/FuctionDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuctionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = GetNumberFromConsole();
+            int n2 = GetNumberFromConsole();
+
+            for(int index=n1;index<n2;index++)
+            {
+                if (IsPrime(index))
+                {
+                    Console.WriteLine(index);
+                }
+
+            }
+
+        }
+
+        private static bool IsPrime(int number)
+        {
+            bool isPrime = true;
+            for (int index = 2; index < number; index++)
+            {
+                if (number % index == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            return isPrime;
+        }
+
+        static int GetNumberFromConsole()
+        {
+            Console.WriteLine("Enter Number");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
