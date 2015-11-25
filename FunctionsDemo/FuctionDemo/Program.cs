78ab0d400d62530e94a7955e8bb7e9e413f2bec2
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2015-11-25 08:05:21 +0530

    Changes

--- FunctionsDemo/FuctionDemo/Program.cs
+++ FunctionsDemo/FuctionDemo/Program.cs
@@ -13,7 +13,43 @@ namespace FuctionDemo
             int n1 = GetNumberFromConsole();
             int n2 = GetNumberFromConsole();
 
+            PrimeNumbers(n1, n2);
+
+            EvenNumbers(n1, n2);
+            
+            Factors(n1, n2);
+
+        }
+
+        private static void Factors(int n1, int n2)
+        {
+            int number = GetNumberFromConsole();
+            Console.WriteLine("Factors");
+            for (int index = n1; index < n2; index++)
+            {
+                if (index % number == 0)
+                {
+                    Console.WriteLine(index);
+                }
+            }
+        }
+
+        private static void EvenNumbers(int n1, int n2)
+        {
+            Console.WriteLine("Even Numbers");
             for(int index=n1;index<n2;index++)
+            {
+                if(index%2==0)
+                {
+                    Console.WriteLine(index);
+                }
+            }
+        }
+
+        private static void PrimeNumbers(int n1, int n2)
+        {
+            Console.WriteLine("Prime Numbers");
+            for (int index = n1; index < n2; index++)
             {
                 if (IsPrime(index))
                 {
@@ -21,7 +57,6 @@ namespace FuctionDemo
                 }
 
             }
-
         }
 
         private static bool IsPrime(int number)
---
+++ MyFirstWebSite/MyFirstWebSite/main.js
@@ -0,0 +1,12 @@
+﻿function submitDate() {
+    var firstname = document.getElementsByName("firstName123");
+
+    if (firstname[0].value.length <= 0) {
+        alert("First Name rEQUIRED");
+    }
+
+
+
+
+    return false;
+}
